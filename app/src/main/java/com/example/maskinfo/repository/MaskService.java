47e4d89c95273526dc9fe0b7bacf77c99bf42471
mjcho1 <alswlwh1224@naver.com>
2020-08-31 11:13:58 +0900

    first commit example mask info app

---
+++ app/src/main/java/com/example/maskinfo/repository/MaskService.java
@@ -0,0 +1,18 @@
+package com.example.maskinfo.repository;
+
+import com.example.maskinfo.model.StoreInfo;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MaskService {
+    String BASE_URL = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/";
+    @GET("/storesByGeo/json/")
+    Call<StoreInfo> fetchStoreInfo();
+}
+
+/*
+public interface GitHubService {
+    @GET("users/{user}/repos")
+    Call<List<Repo>> listRepos(@Path("user") String user);
+}*/
